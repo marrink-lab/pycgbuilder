379e75a398e7c8c3c24a3394fcadb91d1e716cc6
Peter Kroon <p.c.kroon@rug.nl>
2020-09-18 14:03:45 +0200

    Polish setup and requirements

--- pycgbuilder/__main__.py
+++ pycgbuilder/__main__.py
@@ -2,7 +2,12 @@ from PyQt5.QtWidgets import QApplication
 from .interface import CGBuilder
 import sys
 
-qapp = QApplication(sys.argv)
-app = CGBuilder()
-app.show()
-qapp.exec_()
+
+def main():
+    qapp = QApplication(sys.argv)
+    app = CGBuilder()
+    app.show()
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
