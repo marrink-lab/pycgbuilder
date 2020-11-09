8495235d69d38b406738406c83a94c4e97122567
Peter Kroon <peterkroon06+github@gmail.com>
2020-11-09 15:42:07 +0100

    Rescale all coordinates to median bondlength=1

--- pycgbuilder/draw_mol.py
+++ pycgbuilder/draw_mol.py
@@ -10,13 +10,13 @@ def rot(x, y, theta):
     return x*math.cos(theta) - y*math.sin(theta), x*math.sin(theta) + y*math.cos(theta)
 
 
-def make_edge(p0, p1, order, spacing=0.02, sep=0.25):
+def make_edge(p0, p1, order, spacing=0.1, sep=0.2):
     # Step 1: Make a horizontal line from sep to bond_length - sep, and figure
     #         out how far we'll need to rotate it. This line is (x0p, x1p)
     x0, y0 = p0
     x1, y1 = p1
     bond_length = math.sqrt((x0 - x1)**2 + (y0-y1)**2)
-    sep = min(sep * bond_length, sep/5)
+    sep = min(sep * bond_length, sep)
     x0p = sep
     x1p = bond_length - sep
     theta = math.atan2((y1 - y0), (x1 - x0))
@@ -26,8 +26,6 @@ def make_edge(p0, p1, order, spacing=0.02, sep=0.25):
     #         These we then rotate , resulting in ((x0pp, y0pp), (x1pp, y1pp)).
     #         Finally, translate them from the origin to (x0, y0)
     out = []
-    if order == 1.5:
-        order = 2
     for f in range(order):
         y0p = y1p = spacing * ((1-order)/2 + f)
         x0pp, y0pp = rot(x0p, y0p, theta)
@@ -44,8 +42,6 @@ def make_edge(p0, p1, order, spacing=0.02, sep=0.25):
 def draw_molecule(graph, clusters=None, labels=None, edge_widths=None, pos=None, ax=None):
     if not ax:
         ax = plt.gca()
-    if edge_widths is None:
-        widths = {0: 0, 1: 1, 1.5: 1.5, 2: 2, 3: 3, 4: 4}
 
     if labels is None:
         elems = nx.get_node_attributes(graph, 'element')
@@ -66,17 +62,17 @@ def draw_molecule(graph, clusters=None, labels=None, edge_widths=None, pos=None,
         if not order:
             order = 1
         if order == 1.5:
-            tmp = make_edge(pos[idx], pos[jdx], order)
+            tmp = make_edge(pos[idx], pos[jdx], 2)
             arom_edges.append(tmp[0])
             edges.append(tmp[1])
         else:
             edges.extend(make_edge(pos[idx], pos[jdx], order))
-    ax.add_collection(LineCollection(edges, color='black'))
-    ax.add_collection(LineCollection(arom_edges, color='black', linestyle='--'))
+    ax.add_collection(LineCollection(edges, color='black', linewidths=edge_widths))
+    ax.add_collection(LineCollection(arom_edges, color='black', linestyle='dotted', linewidths=edge_widths))
 
     if clusters is not None:
         cmap = plt.get_cmap('tab10')
         for idx, position in pos.items():
             member = clusters[idx]
-            ax.pie(member, center=position, radius=0.3,
+            ax.pie(member, center=position, radius=0.45,
                     colors=[cmap.colors[idx%cmap.N] for idx in range(len(member))])
--- pycgbuilder/embed_molecule.py
+++ pycgbuilder/embed_molecule.py
@@ -1,17 +1,45 @@
 import networkx as nx
+import numpy as np
 import math
 
 
+def rescale_bondlengths(graph, embedding, scale=1, reductor=np.median):
+    keys = list(embedding)
+    positions = [embedding[key] for key in keys]
+    positions = np.array(positions)
+    bonds = []
+    for idx, jdx in graph.edges:
+        length = np.linalg.norm(positions[keys.index(idx)] - positions[keys.index(jdx)])
+        bonds.append(length)
+    min_len = reductor(bonds)
+    mean = np.mean(positions, axis=0)
+    positions -= mean
+    positions *= scale/min_len
+    positions += mean
+    return {key: positions[idx] for idx, key in enumerate(keys)}
+
+
+def rescale(layout_func):
+    def layout(graph, *args, **kwargs):
+        try:
+            pos = layout_func(graph, *args, **kwargs)
+        except:
+            pos = {}
+        return rescale_bondlengths(graph, pos)
+    return layout
+
+
+@rescale
 def vsepr_layout(graph):
     dist = dict(nx.shortest_path_length(graph, weight=None))
     for source, dest_d in dist.items():
         for dest, d in dest_d.items():
             if d % 2 == 1:  # odd
                 # sqrt(1 + (n*x)**2 + n*x**2)
-                n = (d - 1) // 2
-                dist[source][dest] = math.sqrt(1 + 3 * n ** 2 + 3 * n)
+                n = (d + 1)/2
+                dist[source][dest] = math.sqrt(3*n**2 - 3*n + 1)
             else:  # even
-                dist[source][dest] = d // 2 * math.sqrt(3)
+                dist[source][dest] = d / 2 * math.sqrt(3)
     try:
         p0 = nx.planar_layout(graph)
     except nx.NetworkXException:
@@ -20,4 +48,7 @@ def vsepr_layout(graph):
     return pos
 
 
-# vsepr_layout = nx.kamada_kawai_layout
+kamada_kawai_layout = rescale(nx.kamada_kawai_layout)
+spring_layout = rescale(nx.spring_layout)
+spectral_layout = rescale(nx.spectral_layout)
+planar_layout = rescale(nx.planar_layout)
